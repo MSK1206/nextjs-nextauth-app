88baaa9b433a25d35c2981d8247af8d18af03a04
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-10 01:01:49 +0900

    Add: import SignIn

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
-import Form from './components/layout/Form';
+import SignIn from './api/SignIn';
 
 export default function Home() {
   return (
     <main className="hero bg-base-200 flex min-h-screen items-center justify-center">
-      <div className="hero-content flex-col">
+      <div className="hero-content flex-col items-center">
         <div className="text-center">
           <Image
             src={'/mylogo.svg'}
@@ -15,8 +15,8 @@ export default function Home() {
             priority
           />
         </div>
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <Form />
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl">
+          <SignIn />
         </div>
       </div>
     </main>

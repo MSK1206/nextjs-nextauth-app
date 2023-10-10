c7505dab64a76a5f38bf397cc5d961c4a59222aa
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-11 01:43:59 +0900

    Fix: レイアウト変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,25 @@
 import Image from 'next/image';
 import SignIn from './components/SignIn';
 
+
 export default function Home() {
   return (
     <main className="hero bg-base-200 flex min-h-screen items-center justify-center">
       <div className="hero-content flex-col items-center">
-        <div className="text-center">
-          <Image
-            src={'/mylogo.svg'}
-            className="animate-rotate-center"
-            alt="My Logo"
-            width={150}
-            height={150}
-            priority
-          />
-        </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl">
-          <SignIn />
+          <form className="card-body">
+            <div className="flex flex-col items-center justify-center">
+              <Image
+                src={'/mylogo.svg'}
+                className="animate-rotate-center mb-5"
+                alt="My Logo"
+                width={100}
+                height={100}
+                priority
+              />
+              <SignIn />
+            </div>
+          </form>
         </div>
       </div>
     </main>

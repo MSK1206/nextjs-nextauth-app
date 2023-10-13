fd379e5e9359b41644a192c745c79c142927537c
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-13 21:28:43 +0900

    Add: redirect callback

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -15,6 +15,13 @@ const authOptions: NextAuthOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith('/')) return `${baseUrl}${url}`;
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
+  },
 };
 
 export default NextAuth(authOptions);

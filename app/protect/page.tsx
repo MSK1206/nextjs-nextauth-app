5dacaa7d4b9e9d4b05e182fd1ced6acd3c9b40a5
MSK1206 <phantom7216@yahoo.co.jp>
2023-10-13 21:31:04 +0900

    Fix: [...nextauth].ts to redirect callback

--- app/protect/page.tsx
+++ app/protect/page.tsx
@@ -7,7 +7,7 @@ export default function page() {
       <h1>認証後だけしか見れないページ</h1>
       <button
         type="button"
-        onClick={() => signOut({ callbackUrl: '/' })}
+        onClick={() => signOut()}
         className="btn btn-base-100 border border-slate-400 rounded-lg p-1 my-5 w-[250px]"
       >
         <span className="bg-clip-text hover:text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
